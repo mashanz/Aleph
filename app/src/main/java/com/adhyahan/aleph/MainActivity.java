3f7c78467434b8bbb18f7a3a232a0daed42ba460
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2017-10-21 00:56:18 +0700

    Suksess Add Icon Luncher

--- app/src/main/java/com/adhyahan/aleph/MainActivity.java
+++ app/src/main/java/com/adhyahan/aleph/MainActivity.java
@@ -1,13 +1,30 @@
 package com.adhyahan.aleph;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+    }
+
+    public void onClickButtonLogin(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Loading...";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
---
+++ app/src/main/java/com/adhyahan/aleph/PostedFeeds.java
@@ -0,0 +1,13 @@
+package com.adhyahan.aleph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostedFeeds extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posted_feeds);
+    }
+}
